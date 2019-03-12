028227a2cc39c2d786b557c93a0c20e68882e7a0
Tavi <tavitavi@gmail.com>
2019-03-13 00:47:30 +0200

    Hello

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function Footer(){
+    return (
+       <div>
+            Aici e ceva
+       </div>
+    )
+}
+
+export default Footer
---
+++ src/new.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Footer from "Components/Footer"
+
+function thisIsAh1(){
+    return (
+        <div>
+            <h1>This is a title </h1>
+            <Footer/>
+        </div>
+
+    )
+
+
+}
+
+export default thisIsAh1;
